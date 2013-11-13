4853b295cd28afcd1256a31e71424cac9367ca65
Max Kramer <mckramer@gmail.com>
2013-11-13 18:41:18 -0500

    Add buildElements function to build a list of requested elements.

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,13 @@
 ( function (root) {
   root.utils = {
     $: function (selector) {
-      return $('#fixtures').find(selector);
+      return $('#fixtures').find(selector || 'span');
+    },
+    buildElements: function (items, element) {
+      if (!element) { element = 'span'; }
+      return $.map(items, function (value, key) {
+        return '<' + element + '>' + key + '</' + element + '>';
+      });
     },
     clearFixtures: function () {
       $('#fixtures').empty();
