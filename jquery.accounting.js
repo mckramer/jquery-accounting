23966124f56e01eb82226e433cba190f9a5e166b
Max Kramer <mckramer@gmail.com>
2013-11-13 22:06:58 -0500

    Clean up

--- jquery.accounting.js
+++ jquery.accounting.js
@@ -14,49 +14,37 @@
    */
   /*jshint validthis: true */
   function format(method) {
-    
     var $this = $(this)
       , accessor = $this.data('accessor') || $.fn[namespace].defaults.accessor
       , value = $this[accessor]()
     ;
-    
     // Save raw format
     $this.data('raw', value);
     // Update value
     $this[accessor](accounting[method].apply(accounting, [value].concat(slice.call(arguments, 1))));
-    
   }
   
   // API
   $.fn[namespace] = function (method) {
-    
     var allArgs = arguments;
-    
     return this.each( function () {
-      
       var $this = $(this)
         , options = $this.data('options')
         , args
       ;
-      
       if ($.isFunction(accounting[method])) {
-        
         args = slice.call(allArgs);
         if (args.length === 1) {
           args.push(options);
         }
-        
         format.apply(this, args);
-        
       } else {
         $.error('Method (' + method + ') is not supported or does not exist.');
       }
-      
     });
-    
-    
   };
   
+  // Defaults
   $.fn[namespace].defaults = {
     accessor: 'text'
   };
